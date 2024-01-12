860e2f7a49763c0246b5682c0c546ce346b5a7a1
PixelNass <nasserdevart@gmail.com>
2024-01-12 23:09:12 +0400

    Jump ability is done

--- Trijam Game Jam 2024/Assets/Scripts/PlayerController2D.cs
+++ Trijam Game Jam 2024/Assets/Scripts/PlayerController2D.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class PlayerController2D : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
-
     [SerializeField] private float moveSpeed = 8f;
     [SerializeField] private float jumpForce = 10f;
-
     private Vector2 moveInput;
+
+    // Ground Checks
+    [Header("Ground Parameters")]
+    private bool IsGrounded;
+    public Transform groundCheckPoint;
+    public LayerMask whatIsGround;
+    public float groundRadiues = 0.2f;
 
     // Flip
     private bool facingRight = false;
@@ -23,11 +28,16 @@ public class PlayerController2D : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.UpArrow) && IsGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
     }
 
     private void FixedUpdate()
     {
+        IsGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundRadiues, whatIsGround);
+
         PlayerMovement();
     }
 
@@ -47,5 +57,12 @@ public class PlayerController2D : MonoBehaviour
         Vector3 Scaler = transform.localScale;
         Scaler.x *= -1;
         transform.localScale = Scaler;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (groundCheckPoint != null) return;
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(groundCheckPoint.position, groundRadiues);
     }
 }
